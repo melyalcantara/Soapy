3cd3b4525dee1cdbdafdacc83881b33a0e0ad2be
melyalcantara <melissaalcantara02@gmail.com>
2019-03-26 21:27:11 -0400

    Soapy con menu

---
+++ SoapyGame-master/Assets/Scripts/Volumensetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volumensetting : MonoBehaviour
+{
+    public Slider volume;
+    public AudioSource mymusic;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mymusic.volume = volume.value;
+
+    }
+}
